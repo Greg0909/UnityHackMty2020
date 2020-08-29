03f2b7038b67f9c0e86240d1ba1bcd26b6294507
Greg Espinosa Carrillo <grega1196764@gmail.com>
2020-08-29 18:32:14 -0500

    La camara se puede mover y se puede spawnear objetos

---
+++ Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+	public GameObject mesa;
+	private Vector3 puntoDeReferencia;
+
+	int px=2, pz=2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		puntoDeReferencia = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			
+			if(px <= 14)
+			{
+				spawnTable(px, pz);
+				pz+=2;
+				if(pz >= 14 )
+				{
+					pz = 2;
+					px += 2;
+				}
+			}
+				
+		}
+    }
+
+	void spawnTable(float x, float z)
+	{
+		float y = 5.3f;
+		x = puntoDeReferencia.x + x;
+		z = puntoDeReferencia.z + z;
+		Instantiate(mesa, new Vector3(x, y, z), Quaternion.identity);
+	}
+}
